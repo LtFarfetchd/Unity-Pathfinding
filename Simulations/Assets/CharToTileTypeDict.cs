78ee2245299fe2ddc022dc4ccd0566b018b5a53d
Adam Judd <adam.judd@outlook.com.au>
2019-11-28 23:56:18 +1100

    Add map interpretation framework

---
+++ Simulations/Assets/CharToTileTypeDict.cs
@@ -0,0 +1,11 @@
+// a dictionary that can chain necessary operations for convenience
+using System.Collections.Generic;
+using TileTypes = MapInterpreterController.TileTypes;
+
+public class CharToTileTypeDict : Dictionary<char, TileTypes> {
+    // chain add
+    public CharToTileTypeDict cAdd(char key, TileTypes value) {
+        Add(key, value);
+        return this;
+    }
+}
---
+++ Simulations/Assets/MapInterpreterController.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using UnityEngine;
+using CTTDict = CharToTileTypeDict;
+
+public class MapInterpreterController : MonoBehaviour
+{
+    public enum TileTypes {
+        TRAVERSABLE,
+        NON_TRAVERSABLE,
+        OUT_OF_BOUNDS
+    }
+
+    private CTTDict charsToTileTypes;
+
+    private string[] mapFiles;
+    private int currentMap = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // initialise the dictionary
+        charsToTileTypes
+            .cAdd('.', TileTypes.TRAVERSABLE)
+            .cAdd('T', TileTypes.NON_TRAVERSABLE)
+            .cAdd('@', TileTypes.OUT_OF_BOUNDS);
+
+        // create a list of the map paths (relative) and sort it
+        
+
+    }
+
+    private string getNextMapFile() {
+        return mapFiles[++currentMap];
+    }
+
+    public TileTypes[][] parseMapFile(string relativePath) {
+        // fetch the map at the specific path given - to implement?
+        return null;
+    }
+
+    public TileTypes[][] getNextMap() {
+        // fetch the next map in sequence
+        string mapFilePath = getNextMapFile();
+        // open it as a stream
+        StreamReader sr = new StreamReader(mapFilePath);
+        // parse the map and close stream
+        TileTypes[][] parsedMap = parseMapFile(sr.ReadToEnd());
+        sr.Close();
+        return parsedMap;
+    }
+}
