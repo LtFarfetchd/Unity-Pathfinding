7e10031ff692ee41c9c58d4c2b81a202ddc80f04
Adam Judd <adam.judd@outlook.com.au>
2019-11-29 13:45:34 +1100

    Add simulator to begin testing

---
+++ Simulations/Assets/SceneSimulatorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Scenario = MapParserController.Scenario;
+using TileTypes = MapParserController.TileTypes;
+
+
+public class SceneSimulatorController : MonoBehaviour
+{
+    public GameObject mapParser;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // fetch the first scene
+        Scenario[] scens = mapParser.GetComponent<MapParserController>().getNextScenarioSet();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
